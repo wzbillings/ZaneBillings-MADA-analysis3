b81e81c329fa069e2d206da9cbb475ea8e7cf64a
wzbillings <39701545+wzbillings@users.noreply.github.com>
2021-10-11 14:20:32 -0400

    updates analysis script

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -7,6 +7,9 @@
 ###############################
 
 # Load needed packages
+library(tidymodels)
+library(gtsummary)
+library(purrr)
 
 # path to data
 data_location <- here::here("data","processed_data","processeddata.rds")
@@ -18,11 +21,44 @@ mydata <- readRDS(data_location)
 #Data fitting/statistical analysis
 ######################################
 
-# fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
+# A quick fcn to get the tables that I want to report
+reg_tab <- function(mod_list, span_list = NULL) {
+  mods <- purrr::map(mod_list, ~.x$fit)
+  gtsummary::tbl_merge(
+    tbls = purrr::map(mods, gtsummary::tbl_regression),
+    tab_spanner = span_list
+  )
+}
 
-# place results from fit into a data frame with the tidy function
-lmtable <- broom::tidy(lmfit)
+###
+# Normal models: BodyTemp is the outcome of interest
+linear_mod <- linear_reg() |>
+  set_mode("regression") |>
+  set_engine("lm")
+
+# Model 1: RunnyNose only
+temp_mod_simple <- linear_mod |>
+  fit(BodyTemp ~ RunnyNose, data = mydata)
+
+# Model 2: all predictors
+temp_mod_all <- linear_mod |>
+  fit(BodyTemp ~ ., data = mydata)
+
+# Result reports
+temp_mod_tbl <- reg_tab(
+  list(temp_mod_simple, temp_mod_all),
+  c("Runny nose only", "All predictors")
+)
+
+####
+
+###
+# Binomial models: Nausea is the outcome of interest
+# Model 1: RunnyNose only
+
+# Model 2: all predictors
+
+####
 
 #look at fit results
 print(lmtable)
